f35cb993b82d1120228b17efbd83da513790c923
richlloydmiles <richlloydmiles@gmail.com>
2015-02-27 15:11:09 +0200

    Added Jetpack function checks

--- index.php
+++ index.php
@@ -14,19 +14,22 @@ function display_stats($content) {
     'limit'=>-1,
     'post_id'=>$post->ID
 );
-$results = stats_get_csv('postviews', $args);
-ob_start();
-?>
-<span class="views">Views: 
-<?php echo $results[0]['views']; ?>
-</span>
-<?php
-$content .= ob_get_contents();
-ob_end_clean();
+	    if ( class_exists( 'Jetpack' ) && Jetpack::is_module_active( 'stats' ) ) {
+	$results = stats_get_csv('postviews', $args);
+	ob_start();
+		?>
+			<span class="views">Views: 
+				<?php echo $results[0]['views']; ?>
+			</span>
+		<?php
+		$content .= ob_get_contents();
+	ob_end_clean();
+   }
 
 return $content;
     }
-	add_filter('the_content', 'display_stats');
 
+		add_filter('the_content', 'display_stats');
+	
 
 	// array(1) { [0]=> array(1) { ["views"]=> string(2) "27" } }
